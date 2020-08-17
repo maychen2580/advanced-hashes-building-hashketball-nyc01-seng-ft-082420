60f3c374309071ff878c37dec1ba380f22d56559
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-17 02:03:33 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -5,14 +5,125 @@ def game_hash
 home:{
 team_name:"Brooklyn Nets",
 colors:["Black", "White"],
-players:{
-  
-}
-  
+players:[
+  {
+    player_Name:"Alan Anderson",
+    number: 0,
+    shoe: 16,
+    points: 22,
+    rebounds: 12,
+    assists: 12,
+    steals: 3,
+    blocks: 1,
+    slam_dunks: 1
+    },
+    {
+    player_Name:"Reggie Evans",
+    number: 30,
+    shoe: 14,
+    points: 12,
+    rebounds: 12,
+    assists: 12,
+    steals: 12,
+    blocks: 12,
+    slam_dunks: 7 
+    },
+    {
+    player_Name:"Brook Lopez",
+    number: 11,
+    shoe: 17,
+    points: 17,
+    rebounds: 19,
+    assists: 10,
+    steals: 3,
+    blocks: 1,
+    slam_dunks: 15
+    },
+    {
+    player_Name:"Mason Plumlee",
+    number: 1,
+    shoe: 19,
+    points: 26,
+    rebounds: 11,
+    assists: 6,
+    steals: 3,
+    blocks: 8,
+    slam_dunks: 5
+    },
+    {
+    player_Name:"Jason Terry",
+    number: 31,
+    shoe: 15,
+    points: 19,
+    rebounds: 2,
+    assists: 2,
+    steals: 4,
+    blocks: 11,
+    slam_dunks: 1
+    }
+] 
 }
 
     away:{
-      
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players:[
+        {
+    player_Name:"Jeff Adrien",
+    number: 4,
+    shoe: 18,
+    points: 10,
+    rebounds: 1,
+    assists: 1,
+    steals: 2,
+    blocks: 7,
+    slam_dunks: 2
+    },
+    {
+    player_Name:"Bismack Biyombo",
+    number: 0,
+    shoe: 16,
+    points: 12,
+    rebounds: 4,
+    assists: 7,
+    steals: 22,
+    blocks: 15,
+    slam_dunks: 10
+    },
+    {
+    player_Name:"Desagna Diop",
+    number: ,
+    shoe: ,
+    points: ,
+    rebounds: ,
+    assists: ,
+    steals: ,
+    blocks: ,
+    slam_dunks: 
+    },
+    {
+    player_Name:"Alan Anderson",
+    number: ,
+    shoe: ,
+    points: ,
+    rebounds: ,
+    assists: ,
+    steals: ,
+    blocks: ,
+    slam_dunks: 
+    },
+    {
+    player_Name:"Alan Anderson",
+    number: ,
+    shoe: ,
+    points: ,
+    rebounds: ,
+    assists: ,
+    steals: ,
+    blocks: ,
+    slam_dunks: 
+    }
+        ]
     }
   }
 end
