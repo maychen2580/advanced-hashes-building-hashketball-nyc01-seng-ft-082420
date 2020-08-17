6fce3669839eec244e3128c9fc03ceff946bd303
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-17 02:15:21 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,14 @@
 # Write your code here!
+require 'pry'
 
 def game_hash
   {
-home:{
-team_name:"Brooklyn Nets",
-colors:["Black", "White"],
-players:[
+home: {
+team_name: "Brooklyn Nets",
+colors: ["Black", "White"],
+players: [
   {
-    player_Name:"Alan Anderson",
+    player_name:"Alan Anderson",
     number: 0,
     shoe: 16,
     points: 22,
@@ -18,7 +19,7 @@ players:[
     slam_dunks: 1
     },
     {
-    player_Name:"Reggie Evans",
+    player_name:"Reggie Evans",
     number: 30,
     shoe: 14,
     points: 12,
@@ -29,7 +30,7 @@ players:[
     slam_dunks: 7 
     },
     {
-    player_Name:"Brook Lopez",
+    player_name:"Brook Lopez",
     number: 11,
     shoe: 17,
     points: 17,
@@ -40,7 +41,7 @@ players:[
     slam_dunks: 15
     },
     {
-    player_Name:"Mason Plumlee",
+    player_name:"Mason Plumlee",
     number: 1,
     shoe: 19,
     points: 26,
@@ -51,7 +52,7 @@ players:[
     slam_dunks: 5
     },
     {
-    player_Name:"Jason Terry",
+    player_name:"Jason Terry",
     number: 31,
     shoe: 15,
     points: 19,
@@ -62,14 +63,14 @@ players:[
     slam_dunks: 1
     }
 ] 
-}
+},
 
-    away:{
+    away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
-      players:[
+      players: [
         {
-    player_Name:"Jeff Adrien",
+    player_name:"Jeff Adrien",
     number: 4,
     shoe: 18,
     points: 10,
@@ -80,7 +81,7 @@ players:[
     slam_dunks: 2
     },
     {
-    player_Name:"Bismack Biyombo",
+    player_name:"Bismack Biyombo",
     number: 0,
     shoe: 16,
     points: 12,
@@ -91,39 +92,40 @@ players:[
     slam_dunks: 10
     },
     {
-    player_Name:"Desagna Diop",
-    number: ,
-    shoe: ,
-    points: ,
-    rebounds: ,
-    assists: ,
-    steals: ,
-    blocks: ,
-    slam_dunks: 
+    player_name:"DeSagna Diop",
+    number: 2,
+    shoe: 14,
+    points: 24,
+    rebounds: 12,
+    assists: 12,
+    steals: 4,
+    blocks: 5,
+    slam_dunks: 5
     },
     {
-    player_Name:"Alan Anderson",
-    number: ,
-    shoe: ,
-    points: ,
-    rebounds: ,
-    assists: ,
-    steals: ,
-    blocks: ,
-    slam_dunks: 
+    player_name:"Ben Gordon",
+    number: 8,
+    shoe: 15,
+    points: 33,
+    rebounds: 3,
+    assists: 2,
+    steals: 1,
+    blocks: 1,
+    slam_dunks: 0
     },
     {
-    player_Name:"Alan Anderson",
-    number: ,
-    shoe: ,
-    points: ,
-    rebounds: ,
-    assists: ,
-    steals: ,
-    blocks: ,
-    slam_dunks: 
+    player_name: "Kemba Walker",
+    number: 33,
+    shoe: 15,
+    points: 6,
+    rebounds: 12,
+    assists: 12,
+    steals: 7,
+    blocks: 5,
+    slam_dunks: 12
     }
         ]
     }
   }
+
 end
